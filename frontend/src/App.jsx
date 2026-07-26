511926a8ffaea99f59b84701680683010b46b15a
PINISETTI YAMINI <yaminipinisetti2006@gmail.com>
2026-07-26 23:05:15 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import BookRide from "./pages/BookRide";
 import RideHistory from "./pages/RideHistory";
+import TrackRide from "./pages/TrackRide";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/book-ride" element={<BookRide />} />
         <Route path="/ride-history" element={<RideHistory />} />
+        <Route path="/track-ride" element={<TrackRide />} />
       </Routes>
     </BrowserRouter>
   );
