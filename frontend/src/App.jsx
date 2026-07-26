7f09f5e1e44d8f021582a011217c367869c55429
PINISETTI YAMINI <yaminipinisetti2006@gmail.com>
2026-07-26 23:03:30 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import BookRide from "./pages/BookRide";
+import RideHistory from "./pages/RideHistory";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/book-ride" element={<BookRide />} />
+        <Route path="/ride-history" element={<RideHistory />} />
       </Routes>
     </BrowserRouter>
   );
